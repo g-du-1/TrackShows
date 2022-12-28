4c059f072c3c38db9a2d310a32c2f9a33d11d6dd
g-du-1 <dudaszeg1@gmail.com>
2022-12-28 01:59:26 +0000

    Remove boilerplate

--- server/src/main/java/com/gd/trackshows/TrackShowsApplication.java
+++ server/src/main/java/com/gd/trackshows/TrackShowsApplication.java
@@ -2,24 +2,10 @@ package com.gd.trackshows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class TrackShowsApplication {
-	public String testFn() {
-		return "Test";
-	}
-
-	@RequestMapping("/")
-	public String home() {
-		String testStr = testFn();
-		System.out.println(testStr);
-		return "Hello World!";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(TrackShowsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TrackShowsApplication.class, args);
+    }
 }
