a2dcb1d6f2e0a0eab5f6ff9eb3cd6d69cfe445e6
g-du-1 <dudaszeg1@gmail.com>
2023-01-08 13:55:25 +0000

    Rework episode endpoint to use path variables

--- server/src/main/java/com/gd/trackshows/api/trakt/episode/TraktEpisodeController.java
+++ server/src/main/java/com/gd/trackshows/api/trakt/episode/TraktEpisodeController.java
@@ -2,8 +2,8 @@ package com.gd.trackshows.api.trakt.episode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +16,8 @@ public class TraktEpisodeController {
         this.traktEpisodeService = traktEpisodeService;
     }
 
-    @GetMapping
-    public TraktEpisode getEpisode(@RequestParam String id, @RequestParam String season, @RequestParam String episode) {
+    @GetMapping("/{id}/{season}/{episode}")
+    public TraktEpisode getEpisode(@PathVariable String id, @PathVariable String season, @PathVariable String episode) {
         return traktEpisodeService.getEpisode(id, season, episode);
     }
 }
